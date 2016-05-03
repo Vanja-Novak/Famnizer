f0d9a663aa62db6d680a47a4a463f497ba33c9b5
UladzimirBasko <Uladzimir_Basko@senla.eu>
2016-05-03 21:04:40 +0300

    reorganize backend and frontend

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/users', users);
 
 app.use(require('./middleware/errorHandler'));
--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,33 @@
 var app = angular.module('famnizer', ['ui.router', 'growl', 'ngCookies']);
 
+
+app.config([
+
+    '$httpProvider',
+    '$animateProvider',
+    '$compileProvider',
+
+    function($httpProvider, $animateProvider, $compileProvider) {
+
+        var requestInterceptor = function($q){
+            return {
+                'request': function (config) {
+                    if(config.url.indexOf(".") === -1) {
+                        config.url = 'http://127.0.0.1:4000/' + config.url;
+                        // if(config.url.indexOf("login") === -1 ) {
+                        //     config.headers['Authorization'] = 'Basic ' + DataStorageService.get(DataStorageService.configs.Authorization);
+                        // }
+                    }
+
+                    return config || $q.when(config);
+                }
+            }
+        };
+
+        $httpProvider.interceptors.push(requestInterceptor);
+    }
+]);
+
 app.run(function($rootScope, $state) {
     $rootScope.$state = $state;
 
